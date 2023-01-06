9de5b9ff73d1d55d859aecfec6bf8b0db2d833bf
Ernesto <ernesto002@icloud.com>
2023-01-06 12:50:16 -0800

    create routes

--- youtube_clone/src/app.js
+++ youtube_clone/src/app.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar, Feed, VideoDetail, SearchFeed, ChannelDetail } from './components'
+import { Box } from '@mui/material';
 
-const app = () => {
-  return (
-    <div>App</div>
-  )
-}
+
+const app = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default app
